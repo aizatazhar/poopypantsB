aecf534ceb4c7d0f54ddc323f9bd4584f77a39c8
aizatazhar <aizat.azharr@gmail.com>
2022-08-30 21:52:59 +0800

    Use deployed endpoints

--- README.md
+++ README.md
@@ -10,6 +10,15 @@ A web app for quotes and stuff.
 
 -   Clone the repo `git clone https://github.com/aizatazhar/cs3219-otot-task-b.git`
 -   `npm install`
+-   In `index.js`, update the mongoose uri as `mongodb://localhost/cs3219-task-b`
 -   On a terminal, do `nodemon index.js`
 -   On a separate terminal, do `mongosh`
+-   Have fun on postman?
+
+### Running deployed
+
+-   Clone the repo `git clone https://github.com/aizatazhar/cs3219-otot-task-b.git`
+-   `npm install`
+-   In `index.js`, update the mongoose uri
+-   On a terminal, do `nodemon index.js`
 -   Have fun on postman?
--- index.js
+++ index.js
@@ -1,21 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
 const apiRoutes = require("./src/apiRoutes");
-const mongoose = require("mongoose");
-
-const app = express();
 const port = process.env.PORT || 3000;
-
+const app = express();
 app.use(express.json());
 app.use("/api", apiRoutes);
-
 app.get("/", (req, res) => res.send("Quotes with Express and Mongoose"));
-
 app.listen(port, () => {
-    console.log("Running on port " + port);
+    console.log(`Running on port ${port}`);
 });
 
-mongoose.connect("mongodb://localhost/cs3219-task-b", { useNewUrlParser: true });
-
+const mongoose = require("mongoose");
+const uri = `mongodb+srv://aizatazhar:${process.env.MONGODB_PW}@cluster0.bfblgo4.mongodb.net/quotes?retryWrites=true&w=majority`;
+mongoose.connect(uri, { useNewUrlParser: true });
 const db = mongoose.connection;
-// Bind connection to error event (to get notification of connection errors)
+// Bind connection to error event to get notification of connection errors
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
