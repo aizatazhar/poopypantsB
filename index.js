402f1670c49ca72560d0cf23bd0674f75d1bb250
aizatazhar <aizat.azharr@gmail.com>
2022-09-01 09:54:47 +0800

    Add tests for quote APIs

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ const app = express();
 app.use(express.json());
 app.use("/api", apiRoutes);
 app.get("/", (req, res) => res.send("Quotes with Express and Mongoose"));
-app.listen(port, () => {
+module.exports = app.listen(port, () => {
     console.log(`Running on port ${port}`);
 });
 
 const mongoose = require("mongoose");
-const uri = `mongodb+srv://aizatazhar:${process.env.MONGODB_PW}@cluster0.bfblgo4.mongodb.net/quotes?retryWrites=true&w=majority`;
-mongoose.connect(uri, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_TEST, { useNewUrlParser: true });
 const db = mongoose.connection;
 // Bind connection to error event to get notification of connection errors
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
