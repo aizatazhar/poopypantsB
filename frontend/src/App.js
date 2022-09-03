c8e06678395b700f9add26c3cab9f0a9aeca3789
aizatazhar <aizat.azharr@gmail.com>
2022-09-03 20:17:37 +0800

    Implement adding of new quote

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
 import { DeleteIcon } from "@chakra-ui/icons";
 import {
     Button,
+    FormControl,
+    FormLabel,
     Heading,
     IconButton,
+    Input,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
     Spinner,
     Table,
     TableContainer,
@@ -11,6 +21,7 @@ import {
     Th,
     Thead,
     Tr,
+    useDisclosure,
     useToast,
     VStack,
 } from "@chakra-ui/react";
@@ -21,18 +32,31 @@ function App() {
     const [quotes, setQuotes] = useState({});
     const [isLoading, setIsLoading] = useState(true);
     const toast = useToast();
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [quoteToAdd, setQuoteToAdd] = useState({ author: "", text: "" });
 
     useEffect(() => {
         getQuotes();
     }, []);
 
+    const showToast = (title, description, status) => {
+        toast({
+            title,
+            description,
+            status,
+            duration: 5000,
+            isClosable: true,
+        });
+    };
+
     const getQuotes = async () => {
         try {
             setIsLoading(true);
             const response = await axiosInstance.get("/api/quotes");
+            response.data.data.sort().reverse();
             setQuotes(response.data);
         } catch (error) {
-            console.error(error);
+            showToast("An error occurred", error.toString(), "error");
         }
         setIsLoading(false);
     };
@@ -40,16 +64,34 @@ function App() {
     const onDelete = async (id) => {
         try {
             const response = await axiosInstance.delete(`/api/quotes/${id}`);
-            toast({
-                title: response.data.message,
-                description: `Deleted the quote "${response.data.data.text}" by ${response.data.data.author}`,
-                status: "success",
-                duration: 5000,
-                isClosable: true,
-            });
+            showToast(
+                response.data.message,
+                `Deleted the quote "${response.data.data.text}" by ${response.data.data.author}`,
+                "success"
+            );
             getQuotes();
         } catch (error) {
-            console.error(error);
+            showToast("An error occurred", error.toString(), "error");
+        }
+    };
+
+    const addQuote = async () => {
+        try {
+            if (!quoteToAdd.author || !quoteToAdd.text) {
+                return showToast("An error occurred", "Both fields must not be empty", "error");
+            }
+
+            const response = await axiosInstance.post(`/api/quotes`, quoteToAdd);
+            showToast(
+                response.data.message,
+                `Created the quote "${response.data.data.text}" by ${response.data.data.author}`,
+                "success"
+            );
+            setQuoteToAdd({ author: "", text: "" });
+            onClose();
+            getQuotes();
+        } catch (error) {
+            showToast("An error occurred", error.toString(), "error");
         }
     };
 
@@ -72,7 +114,11 @@ function App() {
                         <Tbody>
                             {quotes.data.map((quote) => {
                                 return (
-                                    <Tr key={quote._id}>
+                                    <Tr
+                                        key={quote._id}
+                                        _hover={{
+                                            color: "teal.400",
+                                        }}>
                                         <Td>{quote.author}</Td>
                                         <Td>{quote.text}</Td>
                                         <Td>{new Date(quote.create_date).toLocaleDateString()}</Td>
@@ -92,9 +138,44 @@ function App() {
                 </TableContainer>
             )}
 
-            <Button align="end" colorScheme="green">
+            <Button colorScheme="green" onClick={onOpen}>
                 Add a quote
             </Button>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Add a new Quote</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <FormControl isRequired>
+                            <FormLabel>Author</FormLabel>
+                            <Input
+                                onChange={(event) => {
+                                    quoteToAdd.author = event.target.value;
+                                }}
+                            />
+                        </FormControl>
+
+                        <FormControl isRequired pt={4}>
+                            <FormLabel>Quote</FormLabel>
+                            <Input
+                                onChange={(event) => {
+                                    quoteToAdd.text = event.target.value;
+                                }}
+                            />
+                        </FormControl>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button variant="ghost" mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                        <Button colorScheme="green" onClick={addQuote}>
+                            Create
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </VStack>
     );
 }
