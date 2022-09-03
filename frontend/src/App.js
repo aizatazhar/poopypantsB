10652df3722c09897da5cacd0309b7350c04a338
aizatazhar <aizat.azharr@gmail.com>
2022-09-03 18:43:16 +0800

    Implement fetching of quotes from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import {
     Button,
     Heading,
+    Spinner,
     Table,
     TableContainer,
     Tbody,
@@ -10,39 +11,42 @@ import {
     Tr,
     VStack,
 } from "@chakra-ui/react";
+import useFetchData from "./hooks/useFetchData";
 
 function App() {
-    const quotes = [
-        { author: "Lao Tzu", text: "Nuke everyone.", created_date: "3 September 2022" },
-        { author: "Gandhi", text: "These hoes ain't loyal.", created_date: "3 September 2022" },
-    ];
+    const { response: quotes, isLoading } = useFetchData("");
 
     return (
         <VStack spacing={8}>
             <Heading>Lovely Quotes</Heading>
 
-            <TableContainer w="full">
-                <Table variant="striped" colorScheme="green">
-                    <Thead>
-                        <Tr>
-                            <Th>Author</Th>
-                            <Th>Quote</Th>
-                            <Th>Date added</Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        {quotes.map((quote) => {
-                            return (
-                                <Tr>
-                                    <Td>{quote.author}</Td>
-                                    <Td>{quote.text}</Td>
-                                    <Td>{quote.created_date}</Td>
-                                </Tr>
-                            );
-                        })}
-                    </Tbody>
-                </Table>
-            </TableContainer>
+            {isLoading && <Spinner color="green.500" emptyColor="green.100" size="lg" />}
+            {!isLoading && (
+                <TableContainer w="full" maxH="xl" overflowY="scroll">
+                    <Table variant="striped" colorScheme="green">
+                        <Thead>
+                            <Tr>
+                                <Th>Author</Th>
+                                <Th>Quote</Th>
+                                <Th>Date added</Th>
+                                <Th>Delete</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            {quotes.data.map((quote) => {
+                                return (
+                                    <Tr key={quote._id}>
+                                        <Td>{quote.author}</Td>
+                                        <Td>{quote.text}</Td>
+                                        <Td>{new Date(quote.create_date).toLocaleDateString()}</Td>
+                                        <Td>Delete</Td>
+                                    </Tr>
+                                );
+                            })}
+                        </Tbody>
+                    </Table>
+                </TableContainer>
+            )}
 
             <Button align="end" colorScheme="green">
                 Add a quote
--- frontend/src/components/QuoteItem.js
+++
@@ -1,5 +0,0 @@
-function QuoteItem() {
-    return <div>hello wrold</div>;
-}
-
-export default QuoteItem;
