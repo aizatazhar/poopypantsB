311de92d358d6518d8b151d15df290c7fb2a16bc
aizatazhar <aizat.azharr@gmail.com>
2022-09-03 17:20:12 +0800

    Fix deployed URL in workflow

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const apiRoutes = require("./src/apiRoutes");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const app = express();
 app.use(express.json());
 app.use("/api", apiRoutes);
