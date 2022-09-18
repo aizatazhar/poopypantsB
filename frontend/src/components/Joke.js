1bd0e2c1cbe78b3be97b012e3cddc64a66455f59
aizatazhar <aizat.azharr@gmail.com>
2022-09-18 21:33:29 +0800

    Use AWS lambda to get a sfw joke

--- frontend/src/components/Joke.js
+++ frontend/src/components/Joke.js
@@ -1,11 +1,11 @@
-import { Box, Button, HStack, Spinner, Text, useToast, VStack } from "@chakra-ui/react";
-import axios from "axios";
 import { useEffect, useState } from "react";
+import axios from "axios";
+import { Spinner, VStack, Button, useToast, HStack, Text, Box } from "@chakra-ui/react";
 import axiosInstance from "../lib/axiosInstance";
 
 function Joke({ refreshQuotes }) {
     const jokesAxios = axios.create({
-        baseURL: "https://v2.jokeapi.dev/joke",
+        baseURL: "https://254bc5vkx4plwpncho2ituq2ou0aahpz.lambda-url.ap-southeast-1.on.aws/",
     });
     const [joke, setJoke] = useState({});
     const [isLoading, setIsLoading] = useState(true);
@@ -23,7 +23,7 @@ function Joke({ refreshQuotes }) {
 
     const getJoke = async () => {
         setIsLoading(true);
-        const response = await jokesAxios.get("/Programming");
+        const response = await jokesAxios.get();
         setJoke(response.data);
         console.log(response.data);
         setIsLoading(false);
